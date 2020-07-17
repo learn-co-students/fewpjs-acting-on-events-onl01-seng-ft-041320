3f30aa38e0630ce6281b60eeeb38b0e7eec7d2e5
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-17 18:24:58 -0400

    Done.

--- index.js
+++ index.js
@@ -22,9 +22,6 @@ function moveDodgerRight() {
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft(); } 
-});
-
-document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowRight") {
         moveDodgerRight();
     }
