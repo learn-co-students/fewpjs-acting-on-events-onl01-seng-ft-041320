1c3f06009bc0ddff7040ec16d59efc87edd233c6
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-22 17:05:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+const dodger = document.getElementById("dodger")
+const speed = 5;
+
+function moveDodgerLeft() {
+    let left = parseInt( dodger.style.left.replace("px", "") );
+    left -= speed;
+
+    dodger.style.left = `${left}px`;
+}
+
+function moveDodgerRight() {
+    let left = parseInt( dodger.style.left.replace("px", "") );
+    left += speed;
+
+    dodger.style.left = `${left}px`;
+
+}
